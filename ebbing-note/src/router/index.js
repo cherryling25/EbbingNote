3f67d0ef66ac3dd58c5dc241510a76b1c2dbf4b4
Cherry <cherryling25@outlook.com>
2020-02-08 22:01:28 +0800

    初始化

--- ebbing-note/src/router/index.js
+++ ebbing-note/src/router/index.js
@@ -1,28 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+export const constantRouterMap = [
+  { path: '/', component: () => import('@/views/login')},
+  { path: '/success', component: () => import('@/views/success')},
+  { path: '/error', component: () => import('@/views/error'), hidden: true },
+  {path: '/home', component: () => import('@/views/home')}
+]
 
-const router = new VueRouter({
-  routes
-});
-
-export default router;
+export default new VueRouter({
+  mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap //指定路由列表
+})
--- ebbing-note/src/views/Home.vue
+++ ebbing-note/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <el-container style="height: 700px; border: 1px solid #eee">
+  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1', '3']">
+
+        <el-menu-item index="1">
+        <i class="el-icon-s-flag"></i>
+        <span slot="title">今日复习</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-notebook-1"></i>
+        <span slot="title">文档</span>
+      </el-menu-item>
+
+    </el-menu>
+  </el-aside>
+  
+  
+</el-container>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg: 'home'
+    };
   }
-};
+}; 
 </script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+  
+</style>
