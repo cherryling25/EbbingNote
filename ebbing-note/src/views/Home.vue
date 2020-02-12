f1fdf472aa3a7605e197fa3c6228a6969a8283fc
Cherry <cherryling25@outlook.com>
2020-02-12 11:38:36 +0800

    优化名称

--- ebbing-note/src/views/Home.vue
+++ ebbing-note/src/views/Home.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="home">
     <div class="icon">
-      <el-dropdown trigger="click">
+      <!-- TODO -->
+      <el-dropdown trigger="click">   
         <span style="font-size:14px; cursor: pointer;">
-        <i class="el-icon-circle-plus-outline"></i>
-        新建
+          <i class="el-icon-circle-plus-outline"></i>
+          新建
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item><el-button type="text" @click="dialogFormVisible = true">目录</el-button></el-dropdown-item>
-          <el-dropdown-item divided><el-button type="text" @click="dialogFormVisible1 = true">笔记</el-button></el-dropdown-item>
+          <el-dropdown-item>
+            <el-button type="text" @click="dialogFormVisible = true">目录</el-button>
+          </el-dropdown-item>
+          <el-dropdown-item divided>
+            <el-button type="text" @click="dialogFormVisible1 = true">笔记</el-button>
+          </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
 
       <!-- 弹出框1 -->
       <el-dialog title="目录" :visible.sync="dialogFormVisible" width="30%">
-        <el-form :model="form">
+        <el-form :model="categoryForm">
           <el-form-item label="名称" :label-width="formLabelWidth">
-            <el-input v-model="form.name" autocomplete="off"></el-input>
+            <el-input v-model="categoryForm.name" autocomplete="off"></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -28,20 +33,19 @@
 
       <!-- 弹出框2 -->
       <el-dialog title="新建笔记" :visible.sync="dialogFormVisible1" top="10vh">
-        <el-form :model="form1">
-          <el-form-item label="目录" :label-width="formLabelWidth1">
-            <el-select v-model="form1.categoryId" placeholder="请选择目录">
-              <el-option :label="category.categoryName" :value="category.id" v-for="category in categories" :key="category.id"></el-option>
+        <el-form :model="noteForm">
+          <el-form-item label="目录" :label-width="formLabelWidth">
+            <el-select v-model="noteForm.categoryId" placeholder="请选择目录">
+              <el-option :label="category.categoryName" :value="category.id" v-for="category in categories"
+                :key="category.id"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="名称" :label-width="formLabelWidth1">
-            <el-input v-model="form1.noteName" autocomplete="off"></el-input>
+          <el-form-item label="名称" :label-width="formLabelWidth">
+            <el-input v-model="noteForm.noteName" autocomplete="off"></el-input>
           </el-form-item>
-          <el-form-item label="内容" :label-width="formLabelWidth1">
-            <el-input autocomplete="off" type="textarea"
-                :rows="10"
-                placeholder="请输入内容"
-                v-model="form1.noteContent"></el-input>
+          <el-form-item label="内容" :label-width="formLabelWidth">
+            <el-input autocomplete="off" type="textarea" :rows="10" placeholder="请输入内容" v-model="noteForm.noteContent">
+            </el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
@@ -70,12 +74,12 @@
           <el-submenu v-for="category in categories" :key="category.id" v-bind:index="category.id.toString()">
             <template slot="title"><i class="el-icon-message"></i>{{category.categoryName}}</template>
             <el-menu-item-group>
-                <el-menu-item v-for="note in category.notes" :key="note.id" @click="doSomething(note.id)">{{note.title}}</el-menu-item>
+              <el-menu-item v-for="note in category.notes" :key="note.id" @click="addCategory(note.id)">{{note.title}}
+              </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
       </el-aside>
-
       <el-container style="width: 650px; border: 1px solid #eee;">
         <el-main>
           {{content}}
@@ -90,115 +94,130 @@ import Axios from 'axios';
 export default {
   data() {
     return {
-      userAccountId : this.$route.params.id,
-      categories :[],
+      userAccountId: this.$route.params.id,
+      categories: [],
       content: '',
-      options:[],
       dialogFormVisible: false,
-      form: {
+      dialogFormVisible1: false,
+      formLabelWidth: '70px',
+      categoryForm: {
         name: ''
       },
-      formLabelWidth: '70px',
-
-      dialogFormVisible1: false,
-      form1: {
+      noteForm: {
         categoryId: '',
         noteName: '',
-        noteContent:'',
-      },
-      formLabelWidth1: '70px'
-
+        noteContent: '',
+      }
     }
   },
-  mounted:function(){
-      this.listCategory();
+  mounted: function () {
+    this.listCategory();
   },
   methods: {
     listCategory() {
-    var url = "http://192.168.43.221:8080/ebbingnote/category/list";
-      let requestData = {data: this.userAccountId};
-        Axios.post(url,requestData).then((response)=>{
-                let serverResponse = response.data;
-                if(serverResponse && serverResponse.data != null) {
-                  let array = [];
-                  let serverCategories = serverResponse.data;
-                  for (var index = 0; index < serverCategories.length; index++) {
-                    let documentArray = new Array();
-                    let documentList = serverCategories[index].documentList;
-                    if(documentList != null && documentList.length > 0) {
-                        for(var j=0;j<documentList.length;j++) {
-                            let note = {
-                              id : documentList[j].id,
-                              title : documentList[j].title,
-                              status : documentList[j].status,
-                              createdDate : documentList[j].createdDate
-                            };
-                            documentArray.push(note);
-                        }
-                    }
-                    let category = {
-                      id: serverCategories[index].id,
-                      categoryName: serverCategories[index].categoryName,
-                      notes : documentArray
-                    };
-                    array.push(category);
-                  }
-                  this.categories = array;
-                }
-            }).catch((error)=>{
-                console.log(error);
-            })  
-    },
-   confirmCategory(){
-     var url = "http://192.168.43.221:8080/ebbingnote/category/add";
-      let requestData = {userAccountId:this.userAccountId,categoryName:this.form.name,};
-      console.log(requestData);
-        Axios.post(url,requestData).then((response)=>{
-          if(response.data) {
-            alert('save success');// TODO
-            this.listCategory();
-            this.dialogFormVisible = false;
-          }
-        });
-   },
-   confirmNote(){
-    var url = "http://192.168.43.221:8080/ebbingnote/document/add";
+      var url = "http://192.168.43.221:8080/ebbingnote/category/list";  // TODO
       let requestData = {
-        categoryId:this.form1.categoryId,
-        title:this.form1.noteName,
-        content:this.form1.noteContent
+        data: this.userAccountId
+      };
+      Axios.post(url, requestData).then((response) => {
+        let serverResponse = response.data;
+        if (serverResponse && serverResponse.data != null) {
+          let array = [];
+          let serverCategories = serverResponse.data;
+          for (var index = 0; index < serverCategories.length; index++) {
+            let documentArray = new Array();
+            let documentList = serverCategories[index].documentList;
+            if (documentList != null && documentList.length > 0) {
+              for (var j = 0; j < documentList.length; j++) {
+                let note = {
+                  id: documentList[j].id,
+                  title: documentList[j].title,
+                  status: documentList[j].status,
+                  createdDate: documentList[j].createdDate
+                };
+                documentArray.push(note);
+              }
+            }
+            let category = {
+              id: serverCategories[index].id,
+              categoryName: serverCategories[index].categoryName,
+              notes: documentArray
+            };
+            array.push(category);
+          }
+          this.categories = array;
+        }
+      }).catch((error) => {
+        console.log(error);
+      })
+    },
+    confirmCategory() {
+      var url = "http://192.168.43.221:8080/ebbingnote/category/add";
+      let requestData = {
+        userAccountId: this.userAccountId,
+        categoryName: this.categoryForm.name,
       };
       console.log(requestData);
-        Axios.post(url,requestData).then((response)=>{
-          if(response.data) {
-            alert('save success');// TODO
-            this.listCategory();
-            this.dialogFormVisible1 = false;
-          }
+      Axios.post(url, requestData).then((response) => {
+        if (response.data.data) {
+          this.$notify({
+            message: '创建成功',
+            type: 'success'
+          });
+          this.listCategory();
+          this.dialogFormVisible = false;
+        }else{
+          this.$notify.error({
+          message: '创建失败'
         });
-   },
-    doSomething(noteId){
-      var url = "http://192.168.43.221:8080/ebbingnote/document/detail";
-      let requestData = {data: noteId};
-        Axios.post(url,requestData).then((response)=>{
-          console.log(response);
-          this.content = response.data.data.content;
-        });
+        }
+      });
     },
-      //退出登录
-			logout: function () {
-				var _this = this;
-				this.$confirm('确认退出吗?', '提示', {
-				}).then(() => {
-					sessionStorage.removeItem('user');
-					_this.$router.push('/');
-				}).catch(() => {
+    confirmNote() {
+      var url = "http://192.168.43.221:8080/ebbingnote/document/add";
+      let requestData = {
+        categoryId: this.noteForm.categoryId,
+        title: this.noteForm.noteName,
+        content: this.noteForm.noteContent
+      };
+      console.log(requestData);
+      Axios.post(url, requestData).then((response) => {
+        if (response.data.data) {
+          this.$notify({
+            message: '创建成功',
+            type: 'success'
+          });
+          this.listCategory();
+          this.dialogFormVisible1 = false;
+        }else{
+          this.$notify.error({
+          message: '创建失败'
+        });
+        }
+      });
+    },
+    addCategory(noteId) {
+      var url = "http://192.168.43.221:8080/ebbingnote/document/detail";
+      let requestData = {
+        data: noteId
+      };
+      Axios.post(url, requestData).then((response) => {
+        console.log(response);
+        this.content = response.data.data.content;
+      });
+    },
+    //退出登录
+    logout: function () {
+      var _this = this;
+      this.$confirm('确认退出吗?', '提示', {}).then(() => {
+        sessionStorage.removeItem('user');
+        _this.$router.push('/');
+      }).catch(() => {
 
-				});
-      },
-      flushCom:function(){this.$router.go(0);}
+      });
     }
-}; 
+  }
+};
 </script>
 
 <style>
@@ -234,5 +253,44 @@ export default {
     margin: 19px 10px 20px 20px;
   }
 
+  /* 右键样式 */
+.right-menu {
+  position: fixed;
+  background: #fff;
+  border: solid 1px rgba(0, 0, 0, .2);
+  border-radius: 3px;
+  z-index: 999;
+  display: none;
+}
+.right-menu a {
+  width: 75px;
+  height: 28px;
+  line-height: 28px;
+  text-align: center;
+  display: block;
+  color: #1a1a1a;
+}
+.right-menu a:hover {
+  background: #eee;
+  color: #fff;
+}
+.right-menu {
+  border: 1px solid #eee;
+  box-shadow: 0 0.5em 1em 0 rgba(0,0,0,.1);
+  border-radius: 1px;
+}
+a {
+  color: #333;
+  text-decoration: none;
+}
+.right-menu a {
+  padding: 2px;
+}
+.right-menu a:hover {
+  background: #42b983;
+}
+path {
+  fill: black;
+}
 </style>
 
